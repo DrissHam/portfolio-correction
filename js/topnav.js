560beedf1259c53e70ea3918d19cbcd6805f0e38
Driss <you@example.com>
2021-09-14 16:50:12 +0200

    navbar responsive

---
+++ js/topnav.js
@@ -0,0 +1,13 @@
+console.log("script topnav.js is running...");
+
+const actionButton = document.querySelector('.main-header__button');
+const menu = document.querySelector('.menu');
+
+console.log("Contenu de la variable actionButton : " , actionButton );
+console.log("Contenu de la variable menu : " , menu );
+
+actionButton.addEventListener("click", function(event){
+    console.log(event);
+    menu.classList.toggle("open");
+// menu.style.position = "absolute";
+})
